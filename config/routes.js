99cb870695a4b2e007106b4ce03f5f584bc41e4e
Leila <nb.leila10@gmail.com>
2019-04-06 11:28:46 -0500

    Add register user
    Implement the register function for api/register route
    Add the add method in user-model - to add user to database
    Add the generate token function in the authenticate.js file

--- config/routes.js
+++ config/routes.js
@@ -1,15 +1,45 @@
-const axios = require('axios');
+const axios = require("axios")
+const bcrypt = require("bcryptjs")
 
-const { authenticate } = require('../auth/authenticate');
+const Users = require("../database/user-model")
+
+const { authenticate, generateToken } = require("../auth/authenticate")
 
 module.exports = server => {
-  server.post('/api/register', register);
-  server.post('/api/login', login);
-  server.get('/api/jokes', authenticate, getJokes);
-};
+  server.post("/api/register", register)
+  server.post("/api/login", login)
+  server.get("/api/jokes", authenticate, getJokes)
+}
 
 function register(req, res) {
-  // implement user registration
+  const user = req.body
+
+  if (!user.username || !user.password) {
+    res.status(400).json({
+      errorMessage: "Please provide an email, and a password."
+    })
+  } else {
+    //generate hash from user's password
+    const hash = bcrypt.hashSync(user.password, 12) //2 ^ n times
+
+    //override use.password with hash
+    user.password = hash
+
+    //Create token with user info - Login the user when registering
+    const token = generateToken(user)
+
+    // Add user to database and send back response, with token info
+    Users.add(user)
+      .then(userId => {
+        res.status(201).json({ userId: userId, token: token })
+      })
+      .catch(error => {
+        console.log(error)
+        res.status(500).json({
+          errorMessage: "There was an error saving the new user to the database"
+        })
+      })
+  }
 }
 
 function login(req, res) {
@@ -18,15 +48,15 @@ function login(req, res) {
 
 function getJokes(req, res) {
   const requestOptions = {
-    headers: { accept: 'application/json' },
-  };
+    headers: { accept: "application/json" }
+  }
 
   axios
-    .get('https://icanhazdadjoke.com/search', requestOptions)
+    .get("https://icanhazdadjoke.com/search", requestOptions)
     .then(response => {
-      res.status(200).json(response.data.results);
+      res.status(200).json(response.data.results)
     })
     .catch(err => {
-      res.status(500).json({ message: 'Error Fetching Jokes', error: err });
-    });
+      res.status(500).json({ message: "Error Fetching Jokes", error: err })
+    })
 }
